3ef870ee5abd6b1c01b0c8c5b1d8c2f664fbb27e
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2023-09-07 10:38:33 +0200

    fixed login and booking

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,26 +1,41 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+// import { useNavigate } from "react-router-dom";
+import Data from '../api/Data.json';
+import { useNavigate } from 'react-router-dom'; // Use useNavigate for programmatic navigation
+
+// Define types or interfaces for your data
+interface UserData {
+  id: number;
+  username: string;
+  password: string;
+  role: string;
+  bookings: number[]; // Array of session IDs
+}
 
 const LoginForm: React.FC = () => {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // Use useNavigate for navigation
 
- const handleLogin = () => {
-  // Check if either username or password is empty
-  if (!username || !password) {
-    console.log("All fields are required");
-    return; // Stop further execution if fields are empty
-  }
+  const handleLogin = () => {
+    // Check if either username or password is empty
+    if (!username || !password) {
+      console.log("All fields are required");
+      return; // Stop further execution if fields are empty
+    }
+  
+    // Find the user by username
+    const user: UserData | undefined = Data.Users.find((u) => u.username === username);
+  
+    if (user && user.password === password) {
+      console.log("Login successful");
 
-  // Check for the admin credentials
-  if (username === "admin" && password === "admin") {
-    navigate("/Admin");
-  } else {
-    navigate("/User");
-  }
-};
-
+      // Redirect to BookingPage and pass user data as state
+      navigate('/User', { state: { user } });
+    } else {
+      alert("Wrong username or password");
+    }
+  };
 
   return (
     <div style={{ background: "gray", padding: "1rem" }}>
--- src/components/TimeTable.tsx
+++ src/components/TimeTable.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Data from '../api/Data.json';
+import { useLocation } from 'react-router-dom';
 
 // Define TypeScript types for your data
 interface UserData {
@@ -21,11 +22,14 @@ interface UserData {
 function TimeTable() {
   const [userData, setUserData] = useState<UserData | null>(null);
   const [user, setUser] = useState<UserData['Users'][0] | null>(null);
+  const location = useLocation();
+
+  
 
   useEffect(() => {
     // Use the imported data directly
+    setUser(location.state?.user);
     setUserData(Data);
-    setUser(Data.Users.find((u) => u.username === 'test') || null);
   }, []);
 
   const bookSession = (sessionId: number) => {
@@ -36,9 +40,19 @@ function TimeTable() {
         bookings: [...user.bookings, sessionId],
       };
       setUser(updatedUser);
-      // You should also update the server or data source in a real app
     }
   };
+
+  const unbookSession = (sessionId: number) => {
+    // Simulate unbooking a session by updating the user's 'bookings' array
+    if(user){
+        const updatedUser = {
+            ...user,
+            bookings: user.bookings.filter(id => id !== sessionId),
+        };
+        setUser(updatedUser);
+    }
+  }
 
   return (
     <div>
@@ -50,7 +64,7 @@ function TimeTable() {
                 {session.training} on {session.tid}
               </span>
               {user && user.bookings.includes(session.id) ? (
-                <span> (Booked)</span>
+                <button onClick={() => unbookSession(session.id)}>Unbook</button>
               ) : (
                 <button onClick={() => bookSession(session.id)}>Book</button>
               )}
